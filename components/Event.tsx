b82acf9d9ebc20f3b8a6ee093fa4eb99d3a01295
MorisLuis <73044817+MorisLuis@users.noreply.github.com>
2022-11-28 21:21:05 -0600

    Even item hover

--- components/Event.tsx
+++ components/Event.tsx
@@ -4,6 +4,8 @@ import Image from 'next/image'
 import Link from 'next/link'
 import { FC } from 'react'
 import { Event as IEvent } from '../interfaces/Event'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faRightLong } from '@fortawesome/free-solid-svg-icons'
 
 export const Event: FC<IEvent> = ({ name, image, slug }) => {
 
@@ -24,6 +26,10 @@ export const Event: FC<IEvent> = ({ name, image, slug }) => {
 
                     <div className={styles.title}>
                         <p className={styles.title__text}>{name}</p>
+                        <div className={styles.title__action}>
+                            <p className={styles.title__more}>MAS</p>
+                            <FontAwesomeIcon icon={faRightLong} className={styles.title__icon}/>
+                        </div>
                     </div>
                 </div>
             </Link>
